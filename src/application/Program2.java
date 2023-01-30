09b477906f7a14f3fc564e90f3f3b7f0fe921541
henriqueos92 <henriqueos.92@outlook.com>
2023-01-30 15:20:01 -0300

    Departament insert, update e delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -24,6 +24,25 @@ public class Program2 {
 		for(Department dep : list) {
 			System.out.println(dep);
 		}
+		
+		System.out.println("\n=== TEST 3 : department insert ===");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = "+ newDepartment.getId());
+		
+
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
 		sc.close();
 	}
 
